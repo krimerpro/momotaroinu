a730abef96640fa19f36f27f91fda1d2782319b6
varanustoken <varanustoken@gmail.com>
2022-12-27 20:33:05 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,7 +23,7 @@
                         <div class="mt-5 text-center">
                           <a href="https://t.me/MomotaroInuErc20" class="btn btn-lg btn-gradient-orange btn-glow mb-2 animated" target="_blank" data-animation="fadeInUpShorter" data-animation-delay="1.8s" style="margin-left: 25px;background:red">MAIN CHANNEL</a>
                           <a href="https://medium.com/@DaichiMm/momotaro-inu-%E6%A1%83%E5%A4%AA%E9%83%8E-528644c112f7" class="btn btn-lg btn-gradient-orange btn-glow mb-2 animated" target="_blank" data-animation="fadeInUpShorter" data-animation-delay="1.8s" style="margin-left: 25px;background:red">MEDIUM</a>
-                          <a href="https://www.dextools.io/app/en/ether/pair-explorer/" class="btn btn-lg btn-gradient-orange btn-glow mb-2 animated" target="_blank" data-animation="fadeInUpShorter" data-animation-delay="1.8s" style="margin-left: 25px;background:red">DEXTOOLS</a>
+                          <a href="https://www.dextools.io/app/en/ether/pair-explorer/0xe718bac6b0a0c6820664ddce3fe6ff198d5f3085" class="btn btn-lg btn-gradient-orange btn-glow mb-2 animated" target="_blank" data-animation="fadeInUpShorter" data-animation-delay="1.8s" style="margin-left: 25px;background:red">DEXTOOLS</a>
                         </div>
                         <h5 class="d-block white animated text-center" data-animation="fadeInUpShorter" data-animation-delay="1.6s" style="color:red;font-weight:bold;margin-top:20px;background:aliceblue">LOW TAX : 3% tax fee</h5>
                         <h5 class="d-block white animated text-center" data-animation="fadeInUpShorter" data-animation-delay="1.6s" style="color:red;font-weight:bold;margin-top:20px;background:aliceblue">LOW SUPPLY : 5.000.000 $Momotaro</h5>
